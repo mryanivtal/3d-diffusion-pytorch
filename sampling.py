137be14abb30327b9c4fd785eaea61160cf62a44
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2023-04-10 16:48:08 +0300

    Added sampling folder argunents

--- sampling.py
+++ sampling.py
@@ -6,16 +6,30 @@ from tqdm import tqdm
 import os
 import glob
 from PIL import Image
-import random
+from pathlib import Path
 import argparse
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
 # === Parse arguments ===
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default="./latest.pt")
-parser.add_argument('--target', type=str, default="../datasets/srn_cars/cars_train/a4d535e1b1d3c153ff23af07d9064736")
+parser.add_argument('--imagesdir', type=str, default="../datasets/srn_cars/cars_train/a4d535e1b1d3c153ff23af07d9064736")
+parser.add_argument('--outdir', type=str, default="./samples")
+
 args = parser.parse_args()
+OUTPUT_DIR = args.outdir
+TRAINED_MODEL = args.model
+IMAGES_FOLDER = args.images_folder
+
+
+# === Folders etc. ====
+Path(OUTPUT_DIR).mkdir(exist_ok=True, parents=True)
+assert Path(OUTPUT_DIR).exists(), f'Output path {OUTPUT_DIR} does not exist'
+assert Path(IMAGES_FOLDER).exists(), f'Reference Data path {IMAGES_FOLDER} does not exist'
+assert Path(TRAINED_MODEL).exists(), f'Model path {TRAINED_MODEL} does not exist'
+
 
 # === Prep data ===
 imgsize = 64
@@ -23,24 +37,20 @@ data_imgs = []
 data_Rs = []
 data_Ts = []
 
-for img_filename in sorted(glob.glob(args.target + "/rgb/*.png")):
+for img_filename in sorted(glob.glob(IMAGES_FOLDER + "/rgb/*.png")):
     img = Image.open(img_filename)
     img = img.resize((imgsize, imgsize))
     img = np.array(img) / 255 * 2 - 1
-
     img = img.transpose(2, 0, 1)[:3].astype(np.float32)
     data_imgs.append(img)
-
-    pose_filename = os.path.join(args.target, 'pose', os.path.basename(img_filename)[:-4] + ".txt")
+    pose_filename = os.path.join(IMAGES_FOLDER, 'pose', os.path.basename(img_filename)[:-4] + ".txt")
     pose = np.array(open(pose_filename).read().strip().split()).astype(float).reshape((4, 4))
-
     data_Rs.append(pose[:3, :3])
     data_Ts.append(pose[:3, 3])
 
-data_K = np.array(open(os.path.join(args.target, 'intrinsics',
-                                    os.path.basename(img_filename)[:-4] + ".txt")).read().strip().split()).astype(
-    float).reshape((3, 3))
+data_K = np.array(open(os.path.join(IMAGES_FOLDER, 'intrinsics', os.path.basename(img_filename)[:-4] + ".txt")).read().strip().split()).astype(float).reshape((3, 3))
 data_K = torch.tensor(data_K)
+
 
 # === Prep model ===
 model = XUNet(H=imgsize, W=imgsize, ch=128)
@@ -51,6 +61,7 @@ model.to(device)
 # ckpt = torch.load(args.model)
 # model.load_state_dict(ckpt['model'])
 
+
 # === Create the list of available reference views of the 3d element,start with a single given one (initial reference) ===
 w = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7])
 b = w.shape[0]
@@ -58,8 +69,11 @@ record = [[data_imgs[0][None].repeat(b, axis=0),
            data_Rs[0],
            data_Ts[0]]]
 
+sample_path = Path(OUTPUT_DIR) / Path('0')
+sample_path.mkdir(exist_ok=True)
 os.makedirs(f'sampling/0', exist_ok=True)
-Image.fromarray(((data_imgs[0].transpose(1, 2, 0) + 1) * 127.5).astype(np.uint8)).save('sampling/0/gt.png')
+Image.fromarray(((data_imgs[0].transpose(1, 2, 0) + 1) * 127.5).astype(np.uint8)).save(sample_path / Path('gt.png'))
+
 
 # === Do the diffusion loop (from noise to image etc.) ===
 with torch.no_grad():
@@ -71,9 +85,10 @@ with torch.no_grad():
         img = sample(model, record=record, target_R=R, target_T=T, K=data_K, w=w)
         record.append([img, R.cpu().numpy(), T.cpu().numpy()])
 
-        os.makedirs(f'sampling/{step}', exist_ok=True)
-        Image.fromarray(((gt.transpose(1, 2, 0) + 1) * 127.5).astype(np.uint8)).save(f'sampling/{step}/gt.png')
+        sample_path = Path(OUTPUT_DIR) / Path('step')
+        sample_path.mkdir(exist_ok=True)
+        Image.fromarray(((gt.transpose(1, 2, 0) + 1) * 127.5).astype(np.uint8)).save(sample_path / Path('gt.png'))
         for i in w:
-            Image.fromarray(((img[i].transpose(1, 2, 0) + 1) * 127.5).astype(np.uint8)).save(f'sampling/{step}/{i}.png')
+            Image.fromarray(((img[i].transpose(1, 2, 0) + 1) * 127.5).astype(np.uint8)).save(sample_path / Path(f'{i}.png'))
 
         step += 1
